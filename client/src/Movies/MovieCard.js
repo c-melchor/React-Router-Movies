b0764e6dfec64729543c02e7547af55b28a0cb3b
c_melchor <cah.melchor@gmail.com>
2020-11-09 13:02:43 -0800

    movies rendered

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React from "react";
+import {
+  Route,
+  NavLink,
+  Switch,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
+import MovieDetails from "./MovieList";
 
-export default function MovieCard (props) {
-  return;
+export default function MovieCard(props) {
+  const { movie } = props;
+
+  const { id } = useParams;
+  const item = props.movies.find(item => {
+    item.id === id;
+  });
+
+  const { path, url } = useRouteMatch();
+
+  if (!movies.length) return "...loading";
+
+  return "hello";
 }
